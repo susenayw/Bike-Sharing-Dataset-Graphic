e77e64ef9072df7f50ae2ff3a2d2960169837186
SUWS <146638540+susenayw@users.noreply.github.com>
2026-04-18 17:19:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title('Dashboard Penyewaan Sepeda 🚲')
+
+df = pd.read_csv("dashboard/main_data.csv")
+
+st.subheader('Ringkasan Data')
+total_rentals = df['cnt'].sum()
+st.metric(label="Total Seluruh Penyewaan", value=f"{total_rentals:,}")
+
+st.subheader('Tren Penyewaan Sepeda per Bulan')
+fig, ax = plt.subplots(figsize=(10, 5))
+sns.lineplot(data=df, x='dteday', y='cnt', color='skyblue', ax=ax)
+plt.xticks(rotation=45)
+st.pyplot(fig)
+st.write("Grafik ini menunjukkan bagaimana fluktuasi penyewaan sepeda berubah seiring waktu.")
+
+st.subheader('Jumlah Penyewaan Berdasarkan Kondisi Cuaca')
+fig, ax = plt.subplots(figsize=(10, 5))
+sns.barplot(data=df, x='weathersit', y='cnt', palette='viridis', ax=ax)
+st.pyplot(fig)
+st.write("Dapat dilihat bahwa kondisi cuaca cerah memiliki jumlah penyewaan paling tinggi dibandingkan cuaca lainnya.")
